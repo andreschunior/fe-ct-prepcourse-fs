c0e81d4a9a3135ef6a3a2d8c704cd0b9dbbb92d0
andreschunior <andres23540575@gmail.com>
2024-01-04 15:29:01 -0400

    actualizacion modulo 8 objetos ejercicios terminados

--- M08 Objects + Callbacks/objects/ejercicios/01.js
+++ M08 Objects + Callbacks/objects/ejercicios/01.js
@@ -2,7 +2,7 @@ const obtenerValorPropiedad = (objeto, propiedad) => {
   // Recibe un objeto y el nombre de una propiedad.
   // Devuelve el valor de esta propiedad.
   // Tu código:
-  return objeto.hasOwnProperty(propiedad) ? objeto[propiedad] : false;
+  return objeto.hasOwnProperty(propiedad) ? objeto[propiedad] : undefined;
 }
 
 module.exports = obtenerValorPropiedad;
